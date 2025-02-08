3dd40d2a1c945d392fd15a27b66563f7cb4f36af
DENDENICH <daniil.gol26@yandex.ru>
2025-02-08 21:42:18 +0300

    add custom class config from config and user it is

--- app/main.py
+++ app/main.py
@@ -1,13 +1,18 @@
 import uvicorn
 from fastapi import FastAPI
 
+from config import config
+from api import router
+
 app = FastAPI()
+app.include_router(router, prefix=config.api.prefix)
+
 
 if __name__ == '__main__':
     uvicorn.run(
         app="main:app",
         reload=True,
-        host="0.0.0.0",
-        port=8000,
+        host=config.run.host,
+        port=config.run.port,
         log_level="info"  # Adjust log level as needed
     )
