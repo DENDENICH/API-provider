c6f0f6024b5d0398475b64d4838e74671c8c2a24
DENDENICH <daniil.gol26@yandex.ru>
2025-02-08 20:46:36 +0300

    created main app and requirements

--- app/main.py
+++ app/main.py
@@ -1,1 +1,13 @@
-#app
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+if __name__ == '__main__':
+    uvicorn.run(
+        app="main:app",
+        reload=True,
+        host="0.0.0.0",
+        port=8000,
+        log_level="info"  # Adjust log level as needed
+    )
