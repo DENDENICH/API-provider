f9479010d00a196a7a20b8a8f570775f4090b32c
DENDENICH <daniil.gol26@yandex.ru>
2025-02-09 12:09:16 +0300

    add life span

--- app/config/__init__.py
+++ app/config/__init__.py
@@ -1,1 +1,1 @@
-from conf import config
+from .conf import config
--- app/main.py
+++ app/main.py
@@ -1,16 +1,26 @@
+from contextlib import asynccontextmanager
 import uvicorn
 from fastapi import FastAPI
 
 from config import config
+from db import db_core
 from api import router
 
-app = FastAPI()
-app.include_router(router, prefix=config.api.prefix)
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # startup
+    yield
+    # shutdown
+    await db_core.dispose()
+
+app_main = FastAPI()
+app_main.include_router(router, prefix=config.api.prefix)
 
 
 if __name__ == '__main__':
     uvicorn.run(
-        app="main:app",
+        app="main:app_main",
         reload=True,
         host=config.run.host,
         port=config.run.port,
