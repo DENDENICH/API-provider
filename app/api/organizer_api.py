1453b9e1ce4a9a3e1664359de1970832dddbda2e
DENDENICH <daniil.gol26@yandex.ru>
2025-04-11 02:09:18 +0300

    api: correct openapi schemas and urls add logic in organizer api
    auth:
    add redirect parameter after registry
    
    service:
    add bussines service  for organizer
    
    add base exceptions

--- app/api/organizer_api.py
+++ app/api/organizer_api.py
@@ -1,14 +1,16 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from fastapi import APIRouter, Depends, status
+from fastapi import APIRouter, Depends, Request, status
 from core.db import db_core
 
 from core import settings
 
 from schemas.organizer import (
     OrganizerRegisterRequest,
-    OrganizersResponse
+    OrganizersResponse,
 )
+
+from service.busines_service import OrganizerService
 
 router = APIRouter(
     prefix=settings.api.organizers.prefix,
@@ -18,14 +20,20 @@ router = APIRouter(
 
 @router.post("/register", status_code=status.HTTP_201_CREATED)
 async def register_organizer(
+    request: Request,
     data: OrganizerRegisterRequest, 
     session: AsyncSession = Depends(db_core.session_getter)
 ):
     """Регистрация организации"""
+    organizer_service = OrganizerService(session=session)
+    organizer = await organizer_service.register_company_with_admin(
+        name=data.name,
+        address=data.address,
+        inn=data.inn,
+        bank_details=data.bank_details,
+        user_id=request.state.user_id
+    )
     await session.commit()
 
+    return {"details": "OK"}
 
-@router.get("", response_model=OrganizersResponse)
-async def get_organizers(session: AsyncSession = Depends(db_core.session_getter)):
-    """Получение организации"""
-    pass
