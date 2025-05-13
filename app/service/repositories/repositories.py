5d31b91a7ba9c98d58ca3eedc38fa6a3f690dac0
DENDENICH <daniil.gol26@yandex.ru>
2025-05-13 05:18:09 +0300

    Correct bugs in repository contract - add methods

--- app/service/repositories/repositories.py
+++ app/service/repositories/repositories.py
@@ -65,7 +65,7 @@ class OrganizerRepository(BaseRepository[OrganizerModel]):
 class ContactRepository(BaseRepository[ContractModel]):
     """Репозиторий бизнес логики работы с контрактами"""
     def __init__(
-            self, 
+            self,
             session: AsyncSession,
     ):
         super().__init__(ContractModel, session=session, item=ContractItem)
@@ -79,6 +79,30 @@ class ContactRepository(BaseRepository[ContractModel]):
         result = await self.session.execute(stmt)
         model = result.scalar_one_or_none()
         return self.item(**model.dict, model=model) if model is not None else None
+
+    async def get_supplier_available_company(self, company_id: int) -> List[OrganizerItem]:
+        """Получить поставщиков с которыми заключены контракты"""
+        stmt = (
+            select(OrganizerModel)
+            .join(self.model, self.model.supplier_id == OrganizerModel.id)
+            .where(self.model.company_id == company_id)
+        )
+        result = await self.session.execute(stmt)
+        suppliers = result.scalars().all()
+        return [self.item(**supplier.dict, model=supplier) for supplier in suppliers]
+
+    async def delete(self, supplier_id: int, company_id: int) -> bool:
+        """Удалить контракт по id поставщика"""
+        stmt = (
+            delete(self.model)
+            .where(
+                self.model.supplier_id == supplier_id,
+                self.model.company_id == company_id
+            )
+        )
+        result = await self.session.execute(stmt)
+        # Возвращаем False, если результат изменения = 0
+        return False if result.rowcount == 0 else True
 
 
 
